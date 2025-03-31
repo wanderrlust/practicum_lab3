25867a6fe192e6b84ab91e6891fc1f9aca2d783e
wanderrlust <kuchmakivan@gmail.com>
2025-03-31 19:12:41 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./reset.css"
+
+
+export default function App() {
+	return (
+		<>
+			<h1 style={{ textAlign: "center", color: "teal" }}>Hello world</h1>
+			<p
+				style={{
+					textAlign: "center",
+					color: "darkgreen",
+					fontSize: "20px",
+					fontWeight: "bold",
+				}}
+			>
+				React App has been launched successfully
+			</p>
+		</>
+	);
+}
